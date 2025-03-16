2f6e8fc22dd616538490e16626b5725bfc726878
Daria <jonshilin.gg@gmail.com>
2025-03-16 14:14:23 +0300

    init

---
+++ src/main/java/com/example/jobsyserver/JobsyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobsyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobsyserver/JobsyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobsyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
