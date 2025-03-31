915f64f24248bf8f4048ae02ef53fd0e7e598463
sinemoraa <jonshilin.gg@gmail.com>
2025-03-31 14:49:40 +0300

    feat(mapper): add SpecializationMapper

---
+++ src/main/java/com/example/jobsyserver/mapper/SpecializationMapper.java
@@ -0,0 +1,13 @@
+package com.example.jobsyserver.mapper;
+
+import com.example.jobsyserver.dto.common.SpecializationDto;
+import com.example.jobsyserver.model.Specialization;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface SpecializationMapper {
+
+    @Mapping(source = "category.id", target = "categoryId")
+    SpecializationDto toDto(Specialization specialization);
+}
