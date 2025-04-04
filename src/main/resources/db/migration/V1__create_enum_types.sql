4aed59831b7b55ccf3eef7dec0de66d333a8b6a4
sinemoraa <jonshilin.gg@gmail.com>
2025-04-04 15:29:52 +0300

    refactor: rename PostgreSQL enums to use uppercase values for consistency

--- src/main/resources/db/migration/V1__create_enum_types.sql
+++ src/main/resources/db/migration/V1__create_enum_types.sql
@@ -1,8 +1,8 @@
-CREATE TYPE user_role_enum AS ENUM ('client', 'freelancer', 'admin');
-CREATE TYPE experience_enum AS ENUM ('beginner', 'middle', 'expert');
-CREATE TYPE complexity_enum AS ENUM ('easy', 'medium', 'hard');
-CREATE TYPE payment_type_enum AS ENUM ('hourly', 'fixed');
-CREATE TYPE project_status_enum AS ENUM ('open', 'in_progress', 'completed');
-CREATE TYPE project_application_status_enum AS ENUM ('pending', 'approved', 'declined');
-CREATE TYPE project_duration_enum AS ENUM ('less_than_1_month','less_than_3_months','less_than_6_months');
-CREATE TYPE confirmation_action_enum AS ENUM ('registration', 'password_reset');
+CREATE TYPE user_role_enum AS ENUM ('CLIENT', 'FREELANCER', 'ADMIN');
+CREATE TYPE experience_enum AS ENUM ('BEGINNER', 'MIDDLE', 'EXPERT');
+CREATE TYPE complexity_enum AS ENUM ('EASY', 'MEDIUM', 'HARD');
+CREATE TYPE payment_type_enum AS ENUM ('HOURLY', 'FIXED');
+CREATE TYPE project_status_enum AS ENUM ('OPEN', 'IN_PROGRESS', 'COMPLETED');
+CREATE TYPE project_application_status_enum AS ENUM ('PENDING', 'APPROVED', 'DECLINED');
+CREATE TYPE project_duration_enum AS ENUM ('LESS_THAN_1_MONTH','LESS_THAN_3_MONTHS','LESS_THAN_6_MONTHS');
+CREATE TYPE confirmation_action_enum AS ENUM ('REGISTRATION', 'PASSWORD_RESET');
