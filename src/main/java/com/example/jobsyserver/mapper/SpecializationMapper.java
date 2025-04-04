edaedcd97939b1587b9bfb529db4325d94ffe470
sinemoraa <jonshilin.gg@gmail.com>
2025-04-04 15:41:06 +0300

    refactor: add custom mapping from categoryId to Category in SpecializationMapper

--- src/main/java/com/example/jobsyserver/mapper/SpecializationMapper.java
+++ src/main/java/com/example/jobsyserver/mapper/SpecializationMapper.java
@@ -1,13 +1,25 @@
 package com.example.jobsyserver.mapper;
 
 import com.example.jobsyserver.dto.common.SpecializationDto;
+import com.example.jobsyserver.model.Category;
 import com.example.jobsyserver.model.Specialization;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SpecializationMapper {
-
     @Mapping(source = "category.id", target = "categoryId")
     SpecializationDto toDto(Specialization specialization);
+
+    @Mapping(source = "categoryId", target = "category")
+    Specialization toEntity(SpecializationDto specializationDto);
+
+    default Category map(Long categoryId) {
+        if (categoryId == null) {
+            return null;
+        }
+        Category category = new Category();
+        category.setId(categoryId);
+        return category;
+    }
 }
