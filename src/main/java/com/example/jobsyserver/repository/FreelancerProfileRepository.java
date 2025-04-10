6e6d07af0d41c0b0c3dc22f4f2ec5d7922e85f8d
sinemoraa <jonshilin.gg@gmail.com>
2025-04-10 18:28:18 +0300

    feat: add FreelancerProfileRepository with findByUser method

---
+++ src/main/java/com/example/jobsyserver/repository/FreelancerProfileRepository.java
@@ -0,0 +1,11 @@
+package com.example.jobsyserver.repository;
+
+import com.example.jobsyserver.model.FreelancerProfile;
+import com.example.jobsyserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FreelancerProfileRepository extends JpaRepository<FreelancerProfile, Long> {
+    Optional<FreelancerProfile> findByUser(User user);
+}
