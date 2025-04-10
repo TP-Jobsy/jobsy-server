54ea5b0f773e7d77c25515d3e205c85644e711e8
sinemoraa <jonshilin.gg@gmail.com>
2025-04-10 13:56:46 +0300

    feat: add BadRequestException for handling 400 bad request errors

---
+++ src/main/java/com/example/jobsyserver/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.example.jobsyserver.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends AbstractException {
+    public BadRequestException(String message) {
+        super(message, HttpStatus.BAD_REQUEST.value());
+    }
+}
