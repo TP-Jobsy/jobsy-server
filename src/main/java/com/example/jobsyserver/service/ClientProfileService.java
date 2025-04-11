5412d46c69bc0eef30f00e733637fcb50da07fdc
sinemoraa <jonshilin.gg@gmail.com>
2025-04-11 16:15:55 +0300

    feat: define ClientProfileService interface with crud operations

---
+++ src/main/java/com/example/jobsyserver/service/ClientProfileService.java
@@ -0,0 +1,16 @@
+package com.example.jobsyserver.service;
+
+import com.example.jobsyserver.dto.client.*;
+
+import java.util.List;
+
+public interface ClientProfileService {
+    ClientProfileDto getProfile();
+    ClientProfileDto updateBasic(ClientProfileBasicDto basicDto);
+    ClientProfileDto updateContact(ClientProfileContactDto contactDto);
+    ClientProfileDto updateField(ClientProfileFieldDto fieldDto);
+    ClientProfileDto updateUser(ClientProfileUserDto userDto);
+    void deleteAccount();
+    List<ClientProfileDto> getAllClients();
+    ClientProfileDto getClientProfileById(Long id);
+}
