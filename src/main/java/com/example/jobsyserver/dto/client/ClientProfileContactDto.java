c9e7c039958a4488d5b400ecd74a2b41d5240039
sinemoraa <jonshilin.gg@gmail.com>
2025-04-13 15:02:15 +0300

    refactor: add validation annotations to ClientProfileContactDto

--- src/main/java/com/example/jobsyserver/dto/client/ClientProfileContactDto.java
+++ src/main/java/com/example/jobsyserver/dto/client/ClientProfileContactDto.java
@@ -1,12 +1,16 @@
 package com.example.jobsyserver.dto.client;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 @Schema(description = "Контактные данные профиля заказчика")
 public class ClientProfileContactDto {
 
+    @NotBlank(message = "Ссылка не может быть пустой")
+    @URL(message = "Некорректный URL")
     @Schema(description = "Ссылка для связи или на сайт компании", example = "http://acme-corp.example.com")
     private String contactLink;
 }
