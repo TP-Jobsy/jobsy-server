76f7d02fa98e3ef40124e2fbf9a058a4b37aa1df
sinemoraa <jonshilin.gg@gmail.com>
2025-04-13 14:53:36 +0300

    refactor: map FreelancerProfile entity to complex FreelancerProfileDto structure

--- src/main/java/com/example/jobsyserver/mapper/FreelancerProfileMapper.java
+++ src/main/java/com/example/jobsyserver/mapper/FreelancerProfileMapper.java
@@ -1,31 +1,32 @@
 package com.example.jobsyserver.mapper;
 
 import com.example.jobsyserver.dto.freelancer.FreelancerProfileDto;
-import com.example.jobsyserver.dto.freelancer.FreelancerProfileUpdateDto;
 import com.example.jobsyserver.model.FreelancerProfile;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
+import org.springframework.context.annotation.Primary;
 
 import java.util.List;
 
+@Primary
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface FreelancerProfileMapper {
 
-    @Mapping(source = "user", target = "user")
+    @Mappings({
+            @Mapping(source = "country", target = "basic.country"),
+            @Mapping(source = "city", target = "basic.city"),
+            @Mapping(source = "contactLink", target = "contact.contactLink"),
+            @Mapping(source = "categoryId", target = "about.categoryId"),
+            @Mapping(source = "specializationId", target = "about.specializationId"),
+            @Mapping(source = "experienceLevel", target = "about.experienceLevel"),
+            @Mapping(source = "aboutMe", target = "about.aboutMe"),
+            @Mapping(source = "user", target = "user"),
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "createdAt", target = "createdAt"),
+            @Mapping(source = "updatedAt", target = "updatedAt")
+    })
     FreelancerProfileDto toDto(FreelancerProfile profile);
 
     List<FreelancerProfileDto> toDtoList(List<FreelancerProfile> profiles);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "user", ignore = true)
-    FreelancerProfile toEntity(FreelancerProfileUpdateDto updateDto);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())")
-    @Mapping(target = "user", ignore = true)
-    void updateEntityFromDto(FreelancerProfileUpdateDto updateDto, @MappingTarget FreelancerProfile profile);
 }
