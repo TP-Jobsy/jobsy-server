0cf585af0db387055718f9531fd2ea4c16714359
sinemoraa <jonshilin.gg@gmail.com>
2025-04-13 14:50:59 +0300

    feat: add FreelancerProfileContactDto with validation

---
+++ src/main/java/com/example/jobsyserver/dto/freelancer/FreelancerProfileContactDto.java
@@ -0,0 +1,16 @@
+package com.example.jobsyserver.dto.freelancer;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+@Data
+@Schema(description = "Контактные данные фрилансера")
+public class FreelancerProfileContactDto {
+
+    @NotBlank(message = "Ссылка не может быть пустой")
+    @URL(message = "Некорректный URL")
+    @Schema(description = "Ссылка для связи или портфолио", example = "https://linkedin.com/in/username")
+    private String contactLink;
+}
