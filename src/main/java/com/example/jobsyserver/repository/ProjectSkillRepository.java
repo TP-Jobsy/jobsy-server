888baf3e9f0fd5e29f0a5c361151c76feb9bfe0a
sinemoraa <jonshilin.gg@gmail.com>
2025-04-18 15:44:25 +0300

    feat: add ProjectSkillRepository with method to find all skills by project id

---
+++ src/main/java/com/example/jobsyserver/repository/ProjectSkillRepository.java
@@ -0,0 +1,11 @@
+package com.example.jobsyserver.repository;
+
+import com.example.jobsyserver.model.ProjectSkill;
+import com.example.jobsyserver.model.ProjectSkillId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectSkillRepository extends JpaRepository<ProjectSkill, ProjectSkillId> {
+    List<ProjectSkill> findAllByProjectId(Long projectId);
+}
