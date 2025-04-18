f46aea724d6becf5793253583426955791d765c1
sinemoraa <jonshilin.gg@gmail.com>
2025-04-18 15:45:05 +0300

    refactor: update ProjectApplicationMapper to ignore id and createdAt during entity mapping

--- src/main/java/com/example/jobsyserver/mapper/ProjectApplicationMapper.java
+++ src/main/java/com/example/jobsyserver/mapper/ProjectApplicationMapper.java
@@ -5,11 +5,18 @@ import com.example.jobsyserver.dto.project.ProjectApplicationRequestDto;
 import com.example.jobsyserver.model.ProjectApplication;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.context.annotation.Primary;
 
+@Primary
 @Mapper(componentModel = "spring")
 public interface ProjectApplicationMapper {
-    @Mapping(target = "project", ignore = true)
-    @Mapping(target = "freelancer", ignore = true)
+    @Mappings({
+            @Mapping(target = "project", ignore = true),
+            @Mapping(target = "freelancer", ignore = true),
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "createdAt", ignore = true)
+    })
     ProjectApplication toEntity(ProjectApplicationRequestDto dto);
 
     @Mapping(source = "project.id", target = "projectId")
