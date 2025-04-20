7602cd7093accd596232b5f8fdda8427a77e4a51
sinemoraa <jonshilin.gg@gmail.com>
2025-04-21 01:58:05 +0300

    chore: add application_type_enum to support different types of project applications

--- src/main/resources/db/migration/V1__create_enum_types.sql
+++ src/main/resources/db/migration/V1__create_enum_types.sql
@@ -3,6 +3,7 @@ CREATE TYPE experience_enum AS ENUM ('BEGINNER', 'MIDDLE', 'EXPERT');
 CREATE TYPE complexity_enum AS ENUM ('EASY', 'MEDIUM', 'HARD');
 CREATE TYPE payment_type_enum AS ENUM ('HOURLY', 'FIXED');
 CREATE TYPE project_status_enum AS ENUM ('OPEN', 'IN_PROGRESS', 'COMPLETED');
+CREATE TYPE application_type_enum AS ENUM ('RESPONSE', 'INVITATION');
 CREATE TYPE project_application_status_enum AS ENUM ('PENDING', 'APPROVED', 'DECLINED');
 CREATE TYPE project_duration_enum AS ENUM ('LESS_THAN_1_MONTH','LESS_THAN_3_MONTHS','LESS_THAN_6_MONTHS');
 CREATE TYPE confirmation_action_enum AS ENUM ('REGISTRATION', 'PASSWORD_RESET');
