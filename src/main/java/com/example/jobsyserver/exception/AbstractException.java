79b976c7cfa7f375bbe543e385f7eb9b8a09f404
sinemoraa <jonshilin.gg@gmail.com>
2025-04-27 21:10:45 +0300

    refactor: use RuntimeException message directly in AbstractException

--- src/main/java/com/example/jobsyserver/exception/AbstractException.java
+++ src/main/java/com/example/jobsyserver/exception/AbstractException.java
@@ -1,17 +1,13 @@
 package com.example.jobsyserver.exception;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public abstract class AbstractException extends RuntimeException {
-    private final String message;
     private final int statusCode;
 
-    public AbstractException(String message, int statusCode) {
+    protected AbstractException(String message, int statusCode) {
         super(message);
-        this.message = message;
         this.statusCode = statusCode;
     }
 }
