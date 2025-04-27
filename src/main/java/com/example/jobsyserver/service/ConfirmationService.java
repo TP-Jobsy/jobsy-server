6554ce01cef119327f93368ab503bc9a81379c46
sinemoraa <jonshilin.gg@gmail.com>
2025-04-27 19:56:56 +0300

    feat: add findActiveByEmail method to ConfirmationService

--- src/main/java/com/example/jobsyserver/service/ConfirmationService.java
+++ src/main/java/com/example/jobsyserver/service/ConfirmationService.java
@@ -5,9 +5,11 @@ import com.example.jobsyserver.model.Confirmation;
 import com.example.jobsyserver.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ConfirmationService {
     Confirmation createConfirmationFor(User user, ConfirmationAction action);
     Confirmation validateAndUse(String email, String code, ConfirmationAction action);
     List<Confirmation> deleteExpired(ConfirmationAction action);
+    Optional<Confirmation> findActiveByEmail(String email, ConfirmationAction action);
 }
