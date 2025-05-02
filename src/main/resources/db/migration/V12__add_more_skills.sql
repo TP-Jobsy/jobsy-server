619e0e43e1f13b3e0415163535a9e491fde8750e
sinemoraa <jonshilin.gg@gmail.com>
2025-05-03 00:41:04 +0300

    feat: mount csv data and update copy path

--- src/main/resources/db/migration/V12__add_more_skills.sql
+++ src/main/resources/db/migration/V12__add_more_skills.sql
@@ -1,7 +1,3 @@
 COPY skills(name)
-    FROM 'classpath:db/data/skills_to_import.csv'
-    WITH (
-    FORMAT csv,
-    HEADER true,
-    DELIMITER ','
-    );
+    FROM '/var/lib/postgresql/data/db/data/skills_to_import.csv'
+    WITH (FORMAT csv,HEADER true,DELIMITER ',');
