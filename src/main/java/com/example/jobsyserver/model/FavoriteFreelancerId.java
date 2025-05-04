21b4075e4bf2005aa47b5d1ad1c29c3dc252f4c1
sinemoraa <jonshilin.gg@gmail.com>
2025-05-04 21:40:41 +0300

    feat: add embeddable FavoriteFreelancerId for composite key mapping

---
+++ src/main/java/com/example/jobsyserver/model/FavoriteFreelancerId.java
@@ -0,0 +1,24 @@
+package com.example.jobsyserver.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@EqualsAndHashCode
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteFreelancerId implements Serializable {
+
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column(name = "freelancer_id")
+    private Long freelancerId;
+}
