b6e87e73e986cbd331449af6a94a982a69fab316
sinemoraa <jonshilin.gg@gmail.com>
2025-05-12 00:29:32 +0300

    feat: add DTOs for city and country information with JSON ignore properties

--- src/main/java/com/example/jobsyserver/features/common/validation/dto/CountriesNowResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.jobsyserver.features.common.validation.dto;
-
-import java.util.List;
-
-public record CountriesNowResponse (
-        boolean error,
-        String msg,
-        List<String> data
-) {}
