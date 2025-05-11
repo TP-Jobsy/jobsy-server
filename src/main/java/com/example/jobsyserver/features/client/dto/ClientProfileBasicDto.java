7bea3a5f415340a548736e9fac0360a738595cc0
sinemoraa <jonshilin.gg@gmail.com>
2025-05-11 23:32:23 +0300

    feat: add city and country validation annotations to profile DTOs

--- src/main/java/com/example/jobsyserver/features/client/dto/ClientProfileBasicDto.java
+++ src/main/java/com/example/jobsyserver/features/client/dto/ClientProfileBasicDto.java
@@ -1,5 +1,7 @@
 package com.example.jobsyserver.features.client.dto;
 
+import com.example.jobsyserver.features.common.validation.annotation.CityInCountry;
+import com.example.jobsyserver.features.common.validation.annotation.ValidCountry;
 import com.example.jobsyserver.features.user.dto.PublicUserDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
@@ -7,6 +9,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
+@CityInCountry
 @EqualsAndHashCode(callSuper = true)
 @Schema(description = "Основные данные профиля заказчика")
 public class ClientProfileBasicDto extends PublicUserDto {
@@ -19,6 +22,7 @@ public class ClientProfileBasicDto extends PublicUserDto {
     @Schema(description = "Должность", example = "Менеджер проектов")
     private String position;
 
+    @ValidCountry
     @Schema(description = "Страна", example = "Россия")
     private String country;
 
--- src/main/java/com/example/jobsyserver/features/freelancer/dto/FreelancerProfileBasicDto.java
+++ src/main/java/com/example/jobsyserver/features/freelancer/dto/FreelancerProfileBasicDto.java
@@ -1,5 +1,7 @@
 package com.example.jobsyserver.features.freelancer.dto;
 
+import com.example.jobsyserver.features.common.validation.annotation.CityInCountry;
+import com.example.jobsyserver.features.common.validation.annotation.ValidCountry;
 import com.example.jobsyserver.features.user.dto.PublicUserDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -7,9 +9,11 @@ import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@CityInCountry
 @Schema(description = "Основные данные профиля фрилансера")
 public class FreelancerProfileBasicDto extends PublicUserDto {
 
+    @ValidCountry
     @Schema(description = "Страна", example = "Россия")
     private String country;
 
