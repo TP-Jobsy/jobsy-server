909b327576b1ec198ab59cb6c4f87a542a2e8a49
Stanislav <stas.snegir@yandex.ru>
2025-05-17 01:01:44 +0300

    feat: add findByRole and findByIdAndRole methods

--- src/main/java/com/example/jobsyserver/features/user/repository/UserRepository.java
+++ src/main/java/com/example/jobsyserver/features/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.jobsyserver.features.user.repository;
 
+import com.example.jobsyserver.features.common.enums.UserRole;
 import com.example.jobsyserver.features.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    Optional<User> findByRole(UserRole userRole);
+    Optional<User> findByIdAndRole(Long id, UserRole role);
 }
