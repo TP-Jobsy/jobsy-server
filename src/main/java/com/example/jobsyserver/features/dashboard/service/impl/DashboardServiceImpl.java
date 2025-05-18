c89c5bdf9b177572fa0aa12b57a2c354c37318ba
sinemoraa <jonshilin.gg@gmail.com>
2025-05-18 18:46:01 +0300

    fix: update project detail retrieval to filter responses and invites by pending status

--- src/main/java/com/example/jobsyserver/features/dashboard/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/example/jobsyserver/features/dashboard/service/impl/DashboardServiceImpl.java
@@ -29,8 +29,8 @@ public class DashboardServiceImpl implements DashboardService {
     @Override
     public ProjectDetailDto getClientProjectDetail(Long clientId, Long projectId) {
         var projectDto = projectService.getProjectByIdAndClient(projectId, clientId);
-        var responses = appService.getResponsesForProject(projectId, null);
-        var invites = appService.getInvitationsForProject(projectId, null);
+        var responses = appService.getResponsesForProject(projectId, ProjectApplicationStatus.PENDING);
+        var invites = appService.getInvitationsForProject(projectId, ProjectApplicationStatus.PENDING);
         return ProjectDetailDto.builder()
                 .project(projectDto)
                 .responses(responses)
