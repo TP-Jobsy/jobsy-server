763c16fc96f3f0b9e1acffdd967f2a9ba33f229d
sinemoraa <jonshilin.gg@gmail.com>
2025-05-23 20:05:59 +0300

    feat: enhance confirmation code resend functionality to handle different actions

--- src/main/java/com/example/jobsyserver/features/auth/event/ConfirmationCodeResentEvent.java
+++ src/main/java/com/example/jobsyserver/features/auth/event/ConfirmationCodeResentEvent.java
@@ -1,5 +1,6 @@
 package com.example.jobsyserver.features.auth.event;
 
+import com.example.jobsyserver.features.common.enums.ConfirmationAction;
 import com.example.jobsyserver.features.user.model.User;
 
-public record ConfirmationCodeResentEvent(User user, String confirmationCode) {}
+public record ConfirmationCodeResentEvent(User user, String confirmationCode, ConfirmationAction action) {}
