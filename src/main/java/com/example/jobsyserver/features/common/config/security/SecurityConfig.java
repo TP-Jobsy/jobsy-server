e3188cc00cc56029164b0eeafb60616e736e1b89
sinemoraa <jonshilin.gg@gmail.com>
2025-05-24 03:18:51 +0300

    feat: update CORS configuration to allow dynamic localhost ports

--- src/main/java/com/example/jobsyserver/features/common/config/security/SecurityConfig.java
+++ src/main/java/com/example/jobsyserver/features/common/config/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://jobsyapp.ru," + "http://localhost:58473"));
+        config.setAllowedOrigins(List.of("https://jobsyapp.ru," + "http://localhost:[*]"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         config.setExposedHeaders(List.of("Authorization"));
